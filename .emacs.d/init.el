40099cd3fd30bc5cb1010ea4df7784c833f83da2
Iori ONDA <iori.onda@gmail.com>
2013-03-06 16:50:49 +0900

    Set color theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,3 +26,186 @@
 (set-face-attribute 'default nil :family "Ricty" :height 130)
 (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Ricty"))
 (setq face-font-rescale-alist '(("MeiryoKe_Console" . 1.2)))
+
+;;;外観
+(load-theme 'railscasts t nil)
+
+;スクロールバーを消す
+(set-scroll-bar-mode 'nil)
+ 
+;;;対応する括弧を光らせる
+(setq show-paren-delay 0)
+(setq show-paren-style 'single)
+(show-paren-mode t)
+ 
+;;;行番号の表示
+(global-linum-mode t)
+(setq linum-format "%4d:")
+ 
+;;;バックアップ・オートセーブファイルの作成をやめる
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;;;----------------------------------------------
+;;;パッケージ管理
+;;;package.el
+;;;please execute M-x list-package at first
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;;;popwin.el :pop up window for emacs baffer
+;;M-x package-install RET popwin
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+;;;helm.el
+;;M-x package-install RET helm RET
+(require 'helm)
+(require 'helm-config)
+(helm-mode 1)
+
+(defun my-helm ()
+  (interactive)
+  (helm :sources '(
+                   helm-c-source-buffers-list
+                   helm-c-source-recentf
+                   helm-c-source-files-in-current-dir
+                   helm-c-source-mac-spotlight
+                   helm-c-source-buffer-not-found)
+        :buffer "*my helm*"))
+
+(global-set-key (kbd "C-x b") 'my-helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+(setq helm-samewindow nil)
+(push '("*helm-M-x*") popwin:special-display-config)
+
+;; emacsの終了時に、履歴を保存する
+(remove-hook 'kill-emacs-hook 'helm-c-adaptive-save-history)
+;; ディレイは0.2秒
+(setq helm-input-idle-delay 0.02)
+;; 候補のディレクトリが一つしかない場合に、自動的に展開しない
+(setq helm-ff-auto-update-initial-value nil)
+
+
+;;;powerline
+(require 'powerline)
+(defun arrow-right-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 24 2 1\",
+\". c %s\",
+\"  c %s\",
+\".           \",
+\"..          \",
+\"...         \",
+\"....        \",
+\".....       \",
+\"......      \",
+\".......     \",
+\"........    \",
+\".........   \",
+\"..........  \",
+\"........... \",
+\"............\",
+\"........... \",
+\"..........  \",
+\".........   \",
+\"........    \",
+\".......     \",
+\"......      \",
+\".....       \",
+\"....        \",
+\"...         \",
+\"..          \",
+\".           \",
+\"            \"};"  color1 color2))
+
+
+
+(defun arrow-left-xpm (color1 color2)
+  "Return an XPM right arrow string representing."
+  (format "/* XPM */
+static char * arrow_right[] = {
+\"12 24 2 1\",
+\". c %s\",
+\"  c %s\",
+\"           .\",
+\"          ..\",
+\"         ...\",
+\"        ....\",
+\"       .....\",
+\"      ......\",
+\"     .......\",
+\"    ........\",
+\"   .........\",
+\"  ..........\",
+\" ...........\",
+\"............\",
+\" ...........\",
+\"  ..........\",
+\"   .........\",
+\"    ........\",
+\"     .......\",
+\"      ......\",
+\"       .....\",
+\"        ....\",
+\"         ...\",
+\"          ..\",
+\"           .\",
+\"            \"};"  color2 color1))
+
+(defconst color1 "#0044cc")
+(defconst color2 "#0088cc")
+(defconst color3 "#696969")
+
+(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 'xpm t :ascent 'center))
+(defvar arrow-right-2 (create-image (arrow-right-xpm color2 color3) 'xpm t :ascent 'center))
+(defvar arrow-right-3 (create-image (arrow-right-xpm color3 "None") 'xpm t :ascent 'center))
+(defvar arrow-left-1  (create-image (arrow-left-xpm color2 color1) 'xpm t :ascent 'center))
+(defvar arrow-left-2  (create-image (arrow-left-xpm "None" color2) 'xpm t :ascent 'center))
+
+(setq-default mode-line-format
+ (list  '(:eval (concat (propertize " %* %b " 'face 'mode-line-color-1)
+                        (propertize " " 'display arrow-right-1)))
+        '(:eval (concat (propertize " %Z " 'face 'mode-line-color-2)
+                        (propertize " " 'display arrow-right-2)))
+        '(:eval (concat (propertize " %m " 'face 'mode-line-color-3)
+                        (propertize " " 'display arrow-right-3)))
+
+        ;; Justify right by filling with spaces to right fringe - 16
+        ;; (16 should be computed rahter than hardcoded)
+        '(:eval (propertize " " 'display '((space :align-to (- right-fringe 9)))))
+
+        '(:eval (concat (propertize " " 'display arrow-left-2)
+                        (propertize " %p " 'face 'mode-line-color-2)))
+        '(:eval (concat (propertize " " 'display arrow-left-1)
+                        (propertize "%4l:%2c  " 'face 'mode-line-color-1)))
+)) 
+
+(make-face 'mode-line-color-1)
+(set-face-attribute 'mode-line-color-1 nil
+                    :foreground "#fffacd"
+                    :background color1)
+
+(make-face 'mode-line-color-2)
+(set-face-attribute 'mode-line-color-2 nil
+                    :foreground "#fffacd"
+                    :background color2)
+
+(make-face 'mode-line-color-3)
+(set-face-attribute 'mode-line-color-3 nil
+                    :foreground "#fffacd"
+                    :background color3)
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fffacd"
+                    :background "#000"
+		    :bold t
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :foreground "#fffacd"
+                    :background "#000")

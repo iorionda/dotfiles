b9c55bcd45dac4995697072605f0b0b9ec2b8e89
Iori ONDA <iori.onda@gmail.com>
2013-03-22 16:31:12 +0900

    [from now] 2013/03/22 16:31:12
    diff --git a/.emacs.d/init.el b/.emacs.d/init.el
    index 6b02241..4ed5b73 100644
    --- a/.emacs.d/init.el
    +++ b/.emacs.d/init.el
    @@ -184,6 +184,7 @@
     (add-hook 'after-save-hook
               'executable-make-buffer-file-executable-if-script-p)
    
    +;;保存時に git-now を実行する
     (when (executable-find "git-now")
       (defun git-now-after-save-hook()
         (shell-command "git now")))
    @@ -191,7 +192,6 @@
     (add-hook 'after-save-hook
               'git-now-after-save-hook)
    
    -
     ;;関数名を表示する
     (which-function-mode 1)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -184,13 +184,13 @@
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
 
+;;保存時に git-now を実行する
 (when (executable-find "git-now")
   (defun git-now-after-save-hook()
     (shell-command "git now")))
 
 (add-hook 'after-save-hook
           'git-now-after-save-hook)
-
 
 ;;関数名を表示する
 (which-function-mode 1)

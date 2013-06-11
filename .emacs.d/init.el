94b5a3da74ef4b57d7aac7b74211cf9fed4527b4
Iori ONDA <iori.onda@gmail.com>
2013-06-12 00:12:02 +0900

    Change yasnippet path

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -542,18 +542,18 @@ static char * arrow_right[] = {
 (require 'yasnippet)
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"))
-(yas-global-mode 1)
+(yas/global-mode 1)
 
 ;; 単語展開キーバインド (ver8.0から明記しないと機能しない)
 ;; (setqだとtermなどで干渉問題ありでした)
 ;; もちろんTAB以外でもOK 例えば "C-;"とか
 (custom-set-variables '(yas-trigger-key "TAB"))
 ;; 既存スニペットを挿入する
-(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+(define-key yas/minor-mode-map (kbd "C-x i i") 'yas/insert-snippet)
 ;; 新規スニペットを作成するバッファを用意する
-(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+(define-key yas/minor-mode-map (kbd "C-x i n") 'yas/new-snippet)
 ;; 既存スニペットを閲覧・編集する
-(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+(define-key yas/minor-mode-map (kbd "C-x i v") 'yas/visit-snippet-file)
 
 ;; helm-c-yasnippet
 ;; M-x package-install RET helm-c-yasnippet

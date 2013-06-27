186623128e9cc98b923fbff2d419e13aade75cc4
Iori ONDA <iori.onda@gmail.com>
2013-06-27 10:15:28 +0900

    Add ag settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -284,6 +284,7 @@
     request
     dash-at-point
     js2-mode
+    ag
     ))
 
 (let ((not-installed (loop for x in installing-package-list
@@ -827,6 +828,20 @@ static char * arrow_right[] = {
 (add-hook 'js2-mode-hook
           '(lambda ()
              (setq js2-basic-offset 4)))
+
+;; ag
+(require 'ag)
+(setq ag-highlight-search t)
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
 
 ;;; キーバインド
 (define-key global-map (kbd "C-h") 'delete-backward-char)

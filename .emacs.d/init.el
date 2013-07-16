1ca07c7d621d3c69bcf4f8b65d340352730f3861
Iori ONDA <iori.onda@gmail.com>
2013-07-16 15:42:09 +0900

    Add reopen-file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -906,3 +906,22 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 (setq interprogram-cut-function 'paste-to-osx)
 (setq interprogram-paste-function 'copy-from-osx)
+
+(defun reopen-file ()
+  (interactive)
+  (let ((file-name (buffer-file-name))
+        (old-supersession-threat
+         (symbol-function 'ask-user-about-supersession-threat))
+        (point (point)))
+    (when file-name
+      (fset 'ask-user-about-supersession-threat (lambda (fn)))
+      (unwind-protect
+          (progn
+            (erase-buffer)
+            (insert-file file-name)
+            (set-visited-file-modtime)
+            (goto-char point))
+        (fset 'ask-user-about-supersession-threat
+              old-supersession-threat)))))
+
+(global-set-key (kbd "M-r") 'reopen-file)

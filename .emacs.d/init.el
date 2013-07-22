6d73de21793f3c59d7011ebb4c9f4259f6d409fc
Iori ONDA <iori.onda@gmail.com>
2013-07-22 16:32:40 +0900

    Move package settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -253,62 +253,7 @@
 (setq exec-path (cons (concat (getenv "HOME") "/usr/local/opt/rbenv/shims")
                       (cons (concat (getenv "HOME") "/usr/local/opt/rbenv/bin") exec-path)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;パッケージ管理
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;package.el
-;;;please execute M-x list-package at first
-(require 'package)
-(add-to-list 'package-archives
-     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-     '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
 
-(defvar installing-package-list
-  '(
-    ;; ここに使っているパッケージを書く。
-    popwin
-    helm
-    powerline
-    magit
-    ruby-mode
-    ruby-end
-    ido
-    rinari
-    rspec-mode
-    direx
-    highlight-indentation
-    git-gutter
-    auto-complete
-    coffee-mode
-    flymake
-    flymake-coffee
-    undo-tree
-    markdown-mode
-    smart-compile
-    yasnippet
-    helm-c-yasnippet
-    yasnippet-bundle
-    flymake-ruby
-    google-translate
-    motion-mode
-    request
-    dash-at-point
-    js2-mode
-    ag
-    region-bindings-mode
-    multiple-cursors
-    rhtml-mode
-    ))
-
-(let ((not-installed (loop for x in installing-package-list
-                           when (not (package-installed-p x))
-                           collect x)))
-  (when not-installed
-    (package-refresh-contents)
-    (dolist (pkg not-installed)
-      (package-install pkg))))
 
 ;; google-translate
 (require 'google-translate)
---
+++ .emacs.d/inits/20-package.el
@@ -0,0 +1,56 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;パッケージ管理
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;package.el
+;;;please execute M-x list-package at first
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書く。
+    popwin
+    helm
+    powerline
+    magit
+    ruby-mode
+    ruby-end
+    ido
+    rinari
+    rspec-mode
+    direx
+    highlight-indentation
+    git-gutter
+    auto-complete
+    coffee-mode
+    flymake
+    flymake-coffee
+    undo-tree
+    markdown-mode
+    smart-compile
+    yasnippet
+    helm-c-yasnippet
+    yasnippet-bundle
+    flymake-ruby
+    google-translate
+    motion-mode
+    request
+    dash-at-point
+    js2-mode
+    ag
+    region-bindings-mode
+    multiple-cursors
+    rhtml-mode
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                           when (not (package-installed-p x))
+                           collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+      (package-install pkg))))

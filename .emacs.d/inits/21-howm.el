a1db2f4652e4dcecb7671c520718e7f166f990b2
Iori ONDA <iori.onda@gmail.com>
2013-08-26 11:46:58 +0900

    Add howm settings

---
+++ .emacs.d/inits/21-howm.el
@@ -0,0 +1,10 @@
+;; howmの設定
+(setq howm-menu-lang 'ja)
+(global-set-key "\C-c,," 'howm-menu)
+(mapc
+ (lambda (f)
+   (autoload f
+     "howm" "Hitori Otegaru Wiki Modoki" t))
+ '(howm-menu howm-list-all howm-list-recent
+             howm-list-grep howm-create
+             howm-keyword-to-kill-ring))

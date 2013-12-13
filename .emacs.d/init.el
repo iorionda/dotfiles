6e6f09d44dafe0deb55bc9601219ab4064d4b0ed
Iori ONDA <iori.onda@gmail.com>
2013-12-13 19:37:00 +0900

    Move japanese settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,18 +26,6 @@
        (expand-file-name "~/.emacs.d/elisp"))
        load-path))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;日本語設定
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Localeに合わせた環境の設定
-(set-locale-environment nil)
-(set-language-environment 'Japanese)
-
-(prefer-coding-system 'utf-8)
-(setq default-coding-systems 'utf-8)
-(setq file-name-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-
 ;; "yes or no" を "y or n" に
 (fset 'yes-or-no-p 'y-or-n-p)
 
---
+++ .emacs.d/inits/00-japanese.el
@@ -0,0 +1,11 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;日本語設定
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Localeに合わせた環境の設定
+(set-locale-environment nil)
+(set-language-environment 'Japanese)
+
+(prefer-coding-system 'utf-8)
+(setq default-coding-systems 'utf-8)
+(setq file-name-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)

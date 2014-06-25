f5acf9feae0d198c76843ec7a17663c7f3fcc832
Iori ONDA <iori.onda@gmail.com>
2014-06-25 13:52:50 +0900

    Initial commit

---
+++ .zsh/functions/peco-select-history.zsh
@@ -0,0 +1,18 @@
+################################################################################
+# peco
+################################################################################
+function peco-select-history() {
+    local tac
+    if which tac > /dev/null; then
+        tac="tac"
+    else
+        tac="tail -r"
+    fi
+    BUFFER=$(history 100 | \
+        eval $tac | \
+        peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history

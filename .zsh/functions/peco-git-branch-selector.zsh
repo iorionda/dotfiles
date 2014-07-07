3c5abff7ae59b75d48409a34b65dbec886234598
Iori ONDA <iori.onda@gmail.com>
2014-07-07 11:42:21 +0900

    Apply usable interface

--- .zsh/functions/peco-git-branch-selector.zsh
+++ .zsh/functions/peco-git-branch-selector.zsh
@@ -2,24 +2,21 @@
 # peco-git-branch-selector
 ################################################################################
 function peco-git-branch-selector() {
-    if [ -f ".git" ]; then
-      exit
-    fi
+  local tac
+  if which tac > /dev/null; then
+    tac="tac"
+  else
+    tac="tail -r"
+  fi
 
-    local tac
-    if which tac > /dev/null; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-
-    BUFFER=$(git branch | \
-             grep -v "*" | \
-             eval $tac | \
-             peco --query "$LBUFFER")
-    BUFFER="git checkout${BUFFER}"
+  git rev-parse --git-dir >/dev/null 2>&1
+  if [[ $? == 0 ]]; then
+    BUFFER=$(git branch | grep -v "*" | eval $tac | peco --query "$LBUFFER" | tr -d ' ')
+    BUFFER="git checkout $BUFFER"
     CURSOR=$#BUFFER
-    zle clear-screen
+  fi
+  zle clear-screen
 }
+
 zle -N peco-git-branch-selector
 bindkey '^xg' peco-git-branch-selector
